f45d9032031831c8fc43740ff8b07afcdf9c9dcf
diegomardu <diegomartins836@gmail.com>
2022-12-13 19:40:26 -0300

    Aula 01

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { PrismaService } from './prisma.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    PrismaService
+  ],
+})
+export class AppModule {}
